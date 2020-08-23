d2f2035e77bd91effb00d646dd1e9fa9ff5c3d51
Craig Gilbertson <crvgilbertson@gmail.com>
2020-08-23 04:16:33 +0100

    Added new files in asm and c, structured folders better

---
+++ random/factorial/factorial.c
@@ -0,0 +1,15 @@
+/*
+ * Calling the factorial function defined in assembly.
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+
+uint64_t factorial(uint64_t n);
+
+int main() {
+    for (uint64_t i = 0; i < 20; i++) {
+        printf("factorial(%2lu) = %lu\n", i, factorial(i));
+    }
+    return 0;
+}
